1de27d42b3f6bd37590b49c07b68d1c86338afa8
kavinraj <kavinraj.m@gmail.com>
2024-07-19 14:03:26 +0530

    grpc with http rest clinet implementation done

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 generate: 
-	protoc --go_out=product --go_opt=paths=source_relative --go-grpc_out=product --go-grpc_opt=paths=source_relative product.proto
+	buf generate
+	
--- main.go
+++ main.go
@@ -4,13 +4,29 @@ import (
 	"context"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/kavinrajmani/go-grpc-service/product"
+	"github.com/kavinrajmani/go-grpc-service/user"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type myProductServiceServer struct {
-	product.UnimplementedProductServiceServer
+	product.UnimplementedProductServer
+}
+
+type myUserServiceServer struct {
+	user.UnimplementedUserServer
+}
+
+func (s myUserServiceServer) GetUser(ctx context.Context, req *user.UserRequest) (*user.UserResponse, error) {
+	return &user.UserResponse{
+		Id:    req.Id,
+		Name:  "Devdarshan Kavinraj",
+		Email: "devdarshan@gmail.com",
+	}, nil
 }
 
 func (s myProductServiceServer) CreateProduct(ctx context.Context, req *product.ProductRequest) (*product.ProductResponse, error) {
@@ -25,16 +41,53 @@ func (s myProductServiceServer) CreateProduct(ctx context.Context, req *product.
 }
 
 func main() {
-	lis, err := net.Listen("tcp4", ":8080")
+	log.Println("main function entered")
+	lis, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
-	service := &myProductServiceServer{}
-	product.RegisterProductServiceServer(s, service)
+	log.Println("step 1")
 
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	server := grpc.NewServer()
+	productServer := &myProductServiceServer{}
+	userServer := &myUserServiceServer{}
+
+	log.Println("step 2")
+
+	product.RegisterProductServer(server, productServer)
+	user.RegisterUserServer(server, userServer)
+
+	log.Println("step 3")
+
+	// if err := server.Serve(lis); err != nil {
+	// 	log.Fatalf("failed to serve: %v", err)
+	// }
+
+	log.Println("Serving gRPC on 0.0.0.0:8080")
+	go func() {
+		log.Fatalln(server.Serve(lis))
+	}()
+
+	ctx := context.Background()
+	mux := runtime.NewServeMux()
+	conn, err := grpc.NewClient("0.0.0.0:8080",
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
 	}
+	err = product.RegisterProductHandler(ctx, mux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+	user.RegisterUserHandler(ctx, mux, conn)
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: mux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	gwServer.ListenAndServe()
+
 }
